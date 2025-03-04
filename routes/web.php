3a4b2591119102a1dfb7376c384a9832e57b6761
Humper04 <julianmensink25@gmail.com>
2025-03-04 12:57:43 +0100

    Werkende nieuwsfeed
    Hij heeft nu een werkende nieuwsfeed, de positieve feed is nog in productie

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// News
+Route::get('/fetch-news', [ArticleController::class, 'fetchNews'])->name('articles.fetch');
+Route::get('/news', [ArticleController::class, 'index'])->name('articles.index');
