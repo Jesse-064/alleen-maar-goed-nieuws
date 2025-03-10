5f6b362f4a51d004139773d52625bb9182d77507
Jesse <jesse@device-119.home>
2025-03-10 13:10:09 +0100

    update api op home

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 
 // News
 Route::get('/fetch-news', [ArticleController::class, 'fetchNews'])->name('articles.fetch');
