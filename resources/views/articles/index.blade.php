ffe0342bea2f50fe1cfffea686146050947c01a6
Humper04 <julianmensink25@gmail.com>
2025-03-13 16:44:53 +0100

    Permissions + working login
    Gave 777 permissions to everything (I know it's dangerous)
    Added working login and registration pages

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,9 +1,7 @@
-@extends('layouts.app')
-
-@section('title', $filter === 'positive' ? 'Positief Nieuws' : 'Nieuws')
-
-@section('content')
-    <h1 class="mb-4">{{ $filter === 'positive' ? 'Positief Nieuws' : 'Alle Nieuws' }}</h1>
+<x-layout :title="$filter === 'positive' ? 'Positief Nieuws' : 'Nieuws'">
+    <header class="bg-white mb-4">
+        <h1 class="mb-4">{{ $filter === 'positive' ? 'Positief Nieuws' : 'Alle Nieuws' }}</h1>
+    </header>
 
     <!-- Filter links -->
     <div class="mb-3">
@@ -11,7 +9,7 @@
         <a href="{{ route('articles.index', ['filter' => 'all']) }}" class="btn btn-secondary @if($filter === 'all') disabled @endif">Alle Nieuws</a>
     </div>
 
-    <!-- Knop om alle nieuwsartikelen op te halen -->
+    <!-- Button to fetch all news articles -->
     <form action="{{ route('articles.fetchAll') }}" method="POST" class="mb-3">
         @csrf
         <button type="submit" class="btn btn-success">Haal alle nieuwsartikelen op</button>
@@ -30,4 +28,4 @@
             @endforeach
         </ul>
     @endif
-@endsection
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
 
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 
@@ -17,3 +19,11 @@ Route::get('/about', function () {
 Route::get('/contact', function () {
     return view('contact');
 });
+
+// Auth
+Route::get('/register', [RegisteredUserController::class, 'create']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
+Route::get('/login', [SessionController::class, 'create']);
+Route::post('/login', [SessionController::class, 'store']);
+Route::post('/logout', [SessionController::class, 'destroy']);
