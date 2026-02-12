330f4af4471ad67c56a40832b0184b569523bf63
JulianM <julianmensink25gmail.com>
2026-02-12 15:48:33 +0100

    FIx database permissions and change website name

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>{{ $title ?? 'My Website' }}</title>
+    <title>{{ $title ?? 'Alleen Maar Goed Nieuws' }}</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
@@ -25,7 +25,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="/">My Website</a>
+            <a class="navbar-brand" href="/">Alleen Maar Goed Nieuws</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
             </button>
